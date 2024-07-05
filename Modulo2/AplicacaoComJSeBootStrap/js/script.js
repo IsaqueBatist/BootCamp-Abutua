44198176cfc42f6fc2996b2541d40dafb53640a3
Isaque <isaquebatista400@gmail.com>
2024-07-05 17:59:55 -0300

    closes #5 - Funcionalidade ao Submeter o formulário

--- Modulo2/AplicacaoComJSeBootStrap/js/script.js
+++ Modulo2/AplicacaoComJSeBootStrap/js/script.js
@@ -34,7 +34,7 @@ var categories = [
   { id: 3, name: "Importado" }
 ];
 
-const addNewRow = (prodct) => {
+function addNewRow(prodct) {
   let table = document.getElementById('table')
   let {name} = categories[prodct.category-1]
   table.innerHTML += `
@@ -57,5 +57,23 @@ loadProducts()
 function loadProducts() {
   products.map((product) => addNewRow(product))
 }
+
+document.getElementById('formRegister').addEventListener('submit', function(event) {
+  event.preventDefault()
+  console.log(document.getElementById('checkboxNewProduct').checked)
+  console.log(document.getElementById('checkboxPromotion').checked)
+  let newProduct = {
+    id: products.length+1,
+    name: document.getElementById('inputName3').value,
+    description: document.getElementById('inputDescription3').value,
+    price: document.getElementById('inputPrice3').value,
+    category: document.getElementById("inputClass3").value,
+    promotion: document.getElementById('checkboxPromotion').checked,
+    new: document.getElementById('checkboxNewProduct').checked,
+  }
+  addNewRow(newProduct)
+  products.push(newProduct)
+  document.getElementById('formRegister').reset()
+})
 
 
