a4a7a6c9463243399b0198bd524d711314cecab8
Isaque <isaquebatista400@gmail.com>
2024-07-06 22:13:38 -0300

    closes #12 - Implementação do histórico e validação

--- Modulo2/profileGithub/js/scripts.js
+++ Modulo2/profileGithub/js/scripts.js
@@ -1,16 +1,20 @@
+let userHistory= []
 
 async function getUser() {
   const user = document.getElementById('username').value
   const URL = `https://api.github.com/users/${user}`
+
+  
+  
   $.getJSON(URL, (user) => {
     setUserData(user, true)
+    setHistory(user)
   }).fail(() => {
     setUserData(user, false)
   })
 }
 
 function setUserData(user, status) {
-
   document.getElementById('errormessage').innerHTML = status? "" : `
     <div class='alert alert-danger' role='alert'>
       Usuário não encontrado, erro 404
@@ -22,4 +26,16 @@ function setUserData(user, status) {
   document.getElementById("company").innerHTML = user.company ?user.company: ""
   document.getElementById("avatarurl").innerHTML = status?  `
   <img src=${user.avatar_url} id="avatar_url" alt="avatarURL" width="250" class="shadow rounded">  `: ""
+}
+
+
+function setHistory(user) {
+  if(userHistory.filter((u) => u === user.id).length == 0){
+    document.getElementById("history").innerHTML += `
+    <div class="col">
+          <img src=${user.avatar_url} id="avatar_url" alt="avatarURL" width="125" class="shadow rounded">
+        </div>
+    `
+    userHistory.push(user.id)
+  }
 }
