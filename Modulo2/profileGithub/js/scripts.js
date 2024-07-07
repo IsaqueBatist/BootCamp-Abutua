c1b85be0b69c71a522ad5b9bab6fbb462257cba2
Isaque <isaquebatista400@gmail.com>
2024-07-06 21:52:39 -0300

    closes #11 - Mensagem de erro e refatoramento

--- Modulo2/profileGithub/js/scripts.js
+++ Modulo2/profileGithub/js/scripts.js
@@ -3,14 +3,23 @@ async function getUser() {
   const user = document.getElementById('username').value
   const URL = `https://api.github.com/users/${user}`
   $.getJSON(URL, (user) => {
-    document.getElementById("name").innerHTML = user.name
-    document.getElementById("html-url").innerHTML = user.html_url
-    document.getElementById("company").innerHTML = user.company
-    document.getElementById("avatarurl").innerHTML = `
-    <img src=${user.avatar_url} id="avatar_url" alt="avatarURL" width="250" class="shadow rounded">
+    setUserData(user, true)
+  }).fail(() => {
+    setUserData(user, false)
+  })
+}
+
+function setUserData(user, status) {
+
+  document.getElementById('errormessage').innerHTML = status? "" : `
+    <div class='alert alert-danger' role='alert'>
+      Usuário não encontrado, erro 404
+    </div>
     `
 
-  })
-
-
+  document.getElementById("name").innerHTML = user.name ? user.name : ""
+  document.getElementById("html-url").innerHTML = user.html_url? user.html_url : ""
+  document.getElementById("company").innerHTML = user.company ?user.company: ""
+  document.getElementById("avatarurl").innerHTML = status?  `
+  <img src=${user.avatar_url} id="avatar_url" alt="avatarURL" width="250" class="shadow rounded">  `: ""
 }
