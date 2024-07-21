0a302184fa22ccb71650526b330b7a89eb6d0c16
Isaque <isaquebatista400@gmail.com>
2024-07-21 16:53:24 -0300

    configurando currency e locale

--- Modulo3/cap3/cadastroProdutoAngular/src/app/app.component.ts
+++ Modulo3/cap3/cadastroProdutoAngular/src/app/app.component.ts
@@ -4,7 +4,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { FormComponent } from './components/form/form.component';
 import { TableComponent } from './components/table/table.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LOCALE_ID } from '@angular/core';
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
 
+
+registerLocaleData(localePt, 'pt')
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -16,7 +21,10 @@ import { FooterComponent } from './components/footer/footer.component';
     FooterComponent
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    {provide: LOCALE_ID, useValue: 'pt'}
+  ]
 })
 export class AppComponent {
   title = 'cadastroProdutoAngular';
